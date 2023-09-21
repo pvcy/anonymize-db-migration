2a6db9d65c5dd13cd26cdbd2dc36edea6a64c585
John Craft <jcraft@hey.com>
2023-09-21 14:11:51 -0700

    Initial commit

---
+++ users-api/src/server.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const express = require("express");
+const cors = require("cors");
+const PORT = 8080;
+const HOST = "0.0.0.0";
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  "postgres",
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: "db",
+    dialect: "postgres",
+  }
+);
+
+const User = sequelize.define(
+  "user",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false,
+  }
+);
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/users", async (req, res) => {
+  const page = parseInt(req.query.page, 10) || 1;
+  const pageSize = 25;
+  try {
+    const users = await User.findAll({
+      offset: (page - 1) * pageSize,
+      limit: pageSize,
+    });
+
+    res.json({
+      totalUsers: users.length,
+      page: page,
+      pageSize: pageSize,
+      users: users,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to retrieve users" });
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
